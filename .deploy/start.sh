f8e890dbf9a2c16e174e5e1ca8d272f694da21a4
Robert <robert.p1991@gmail.com>
2020-02-24 19:44:19 +0100

    Change name of django app from website to home

--- .deploy/start.sh
+++ .deploy/start.sh
@@ -2,7 +2,7 @@ set -x
 
 export ENV_REGEX="(\w+)=(.+)"
 export ENV_PATH=".env.dist"
-export DEPLOY_PATH_SERVER="websites/youmezurich"
+export DEPLOY_PATH_SERVER="websites/COOKIECUTTER_PLACEHOLDER_PROJECTNAME"
 
 # Mind: The context runs at root directory
 sh ./.deploy/replace_dotenv.sh &&
