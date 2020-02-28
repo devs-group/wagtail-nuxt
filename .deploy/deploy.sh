3abfdbb3dffa960dbd6b0b2ede879f1a8e01a721
Vince Vice <buk6tq5ahz@bzrvp.anonbox.net>
2020-02-28 16:34:17 +0100

    Add: Adaption of CI

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,4 +1,4 @@
 export SSHPASS=${SSH_PASS_NETCUP_SERVER}
-sshpass -e rsync -e "ssh -o StrictHostKeyChecking=no" -avzP ./docker-compose.production.yml ${SERVER_SSH_ADDRESS}:${DEPLOY_PATH_SERVER}/docker-compose.yml
-sshpass -e rsync -e "ssh -o StrictHostKeyChecking=no" --ignore-existing --chmod=u+rw -avzP ./.env.dist ${SERVER_SSH_ADDRESS}:${DEPLOY_PATH_SERVER}/.env
-sshpass -e ssh -tt ${SERVER_SSH_ADDRESS} "cd ${DEPLOY_PATH_SERVER} && docker login ${GITLAB_REGISTRY} -u ${CI_USER_NETCUP} -p ${CI_PASS_NETCUP} && docker-compose pull && docker-compose up -d"
+sshpass -e rsync -e "ssh -o StrictHostKeyChecking=no" -avzP ./docker-compose.production.yml ${STAGING_SERVER_SSH_ADDRESS}:${DEPLOY_PATH_SERVER}/docker-compose.yml
+sshpass -e rsync -e "ssh -o StrictHostKeyChecking=no" --ignore-existing --chmod=u+rw -avzP ./.env.dist ${STAGING_SERVER_SSH_ADDRESS}:${DEPLOY_PATH_SERVER}/.env
+sshpass -e ssh -tt ${STAGING_SERVER_SSH_ADDRESS} "cd ${DEPLOY_PATH_SERVER} && docker login ${GITLAB_REGISTRY} -u ${CI_USER_NETCUP} -p ${CI_PASS_NETCUP} && docker-compose pull && docker-compose up -d"
