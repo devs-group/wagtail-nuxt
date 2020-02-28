1f9c81fa888d779ffd186018146d1d53de988602
Vince Vice <buk6tq5ahz@bzrvp.anonbox.net>
2020-02-28 13:22:44 +0100

    Add: Schema to fetch infocard and image

--- cms/api/schema.py
+++ cms/api/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from home.models import HomePage
+from home.models import HomePage, InfoCard
 
 
 class HomePageNode(DjangoObjectType):
@@ -9,12 +9,35 @@ class HomePageNode(DjangoObjectType):
         model = HomePage
 
 
+class InfoCardNode(DjangoObjectType):
+    image = graphene.String(width=graphene.Int())
+
+    def resolve_image(self, info, width=860):
+        if not self.image:
+            return ""
+
+        return self.image.get_rendition('width-%s' % width).url
+
+    class Meta:
+        model = InfoCard
+
+
 class Query(graphene.ObjectType):
-    # home
+    # HomePage
     home_page = graphene.Field(HomePageNode)
 
     def resolve_home_page(self, info):
         return HomePage.objects.first()
+
+    # InfoCard
+    info_card = graphene.Field(InfoCardNode, id=graphene.Int())
+    info_cards = graphene.List(InfoCardNode)
+
+    def resolve_info_card(self, info, id):
+        return InfoCard.objects.get(pk=id)
+
+    def resolve_info_cards(self, info):
+        return InfoCard.objects.all()
 
 
 schema = graphene.Schema(query=Query)
